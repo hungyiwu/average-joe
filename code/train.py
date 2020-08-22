82058604055a3191bad4a203660f7b8c6eac7e49
Hung-Yi Wu <hungyiwu@protonmail.com>
2020-08-22 16:42:05 -0400

    on-the-fly shuffle

--- code/train.py
+++ code/train.py
@@ -2,53 +2,59 @@ import json
 import shutil
 from pathlib import Path
 
-import numpy as np
+from tensorflow.keras.datasets import mnist
 
 import autoencoder
+import data_util
 
 
 # paths
-data_fp = "./saved_data"
 checkpoint_fp = "./checkpoints"
 
 # params
 latent_dim = 2
-train_params = dict(batch_size=128, epochs=10, validation_split=0.3, shuffle=True)
+num_shuffle = 10
+train_params = dict(batch_size=128, epochs=1, validation_split=0.3, shuffle=True)
 opt = "adam"
 
 # load data
-data_fp = Path(data_fp)
-xt = np.load(data_fp / "x_train.npy")
-xt2 = np.load(data_fp / "x_train_shuffled.npy")
-xe = np.load(data_fp / "x_test.npy")
-xe2 = np.load(data_fp / "x_test_shuffled.npy")
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train = data_util.preprocess(x_train)
+x_test = data_util.preprocess(x_test)
 
 # prep model
-input_shape = xt.shape[1:]
+input_shape = x_train.shape[1:]
 model = autoencoder.conv_ae(input_shape=input_shape, latent_dim=latent_dim)
 model.compile(optimizer=opt)
 
 # train, evaluate
 print("pre-train eval")
-model.evaluate(x=xe, y=xe2, batch_size=train_params["batch_size"])
+model.evaluate(
+    x=x_test, y=data_util.shuffle_label(x_test, y_test), batch_size=train_params["batch_size"],
+)
 
 print("train")
-model.fit(x=xt, y=xt2, **train_params)
+for _ in range(num_shuffle):
+    model.fit(
+        x=x_train, y=data_util.shuffle_label(x_train, y_train), **train_params,
+    )
 
 print("post-train eval")
-model.evaluate(x=xe, y=xe2, batch_size=train_params["batch_size"])
+model.evaluate(
+    x=x_test, y=data_util.shuffle_label(x_test, y_test), batch_size=train_params["batch_size"],
+)
 
 # save model and metadata
 checkpoint_fp = Path(checkpoint_fp)
 if checkpoint_fp.exists():
     shutil.rmtree(checkpoint_fp)
 
-ne = train_params["epochs"]
 model.save_weights(checkpoint_fp / "weights")
 
 config = dict(
     input_shape=input_shape,
     latent_dim=latent_dim,
+    num_shuffle=num_shuffle,
     train_params=train_params,
     optimizer=opt,
 )
