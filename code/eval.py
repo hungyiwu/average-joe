abe95539b0ad68959f0eb94b7fbf11afbfb8cef1
Hung-Yi Wu <hungyiwu@protonmail.com>
2020-08-22 15:53:09 -0400

    plot train result

--- code/eval.py
+++ code/eval.py
@@ -24,18 +24,45 @@ model.load_weights(checkpoint_fp / "weights")
 
 # load data
 data_fp = Path(data_fp)
+xt = np.load(data_fp / "x_train.npy")
+xt_shuffled = np.load(data_fp / "x_train_shuffled.npy")
+yt = np.load(data_fp / "y_train.npy")
 xe = np.load(data_fp / "x_test.npy")
 ye = np.load(data_fp / "y_test.npy")
 
-# test
-xe2 = model.decoder(model.encoder(xe))
+# predict
+xt_pred = model.decoder(model.encoder(xt))
+xe_pred = model.decoder(model.encoder(xe))
 
 # plot settings
 colormap = "gray"
 figure_fp = Path(figure_fp)
-
-# plot
 imshow_param = dict(cmap=colormap, vmin=0, vmax=1)
+
+# plot training
+for digit in [2, 3, 4]:
+    index = np.argwhere(yt == digit)[:, 0]
+    fig, axes = plt.subplots(ncols=3, nrows=3, figsize=(6.4, 6.4))
+
+    for row in range(3):
+        i = index[row]
+        axes[row, 0].imshow(xt[i, ...], **imshow_param)
+        axes[row, 1].imshow(xt_shuffled[i, ...], **imshow_param)
+        axes[row, 2].imshow(xt_pred[i, ...], **imshow_param)
+
+    for ax in axes.reshape(-1):
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+
+    axes[0, 0].set_title("input")
+    axes[0, 1].set_title("target")
+    axes[0, 2].set_title("predict")
+
+    fig.tight_layout()
+    plt.savefig(figure_fp / f"train_{digit}.png")
+    plt.close()
+
+# plot evaluation
 for digit in np.unique(ye):
     index = np.argwhere(ye == digit)[:, 0]
     fig, axes = plt.subplots(ncols=2, nrows=3, figsize=(4.8, 6.4))
@@ -43,15 +70,15 @@ for digit in np.unique(ye):
     for row in range(3):
         i = index[row]
         axes[row, 0].imshow(xe[i, ...], **imshow_param)
-        axes[row, 1].imshow(xe2[i, ...], **imshow_param)
+        axes[row, 1].imshow(xe_pred[i, ...], **imshow_param)
 
     for ax in axes.reshape(-1):
         ax.get_xaxis().set_visible(False)
         ax.get_yaxis().set_visible(False)
 
-    axes[0, 0].set_title("encoder input")
-    axes[0, 1].set_title("decoder output")
+    axes[0, 0].set_title("input")
+    axes[0, 1].set_title("predict")
 
     fig.tight_layout()
-    plt.savefig(figure_fp / f"digit_{digit}.png")
+    plt.savefig(figure_fp / f"eval_{digit}.png")
     plt.close()
